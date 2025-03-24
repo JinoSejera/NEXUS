93f541aa8a28b5e73ff2ce513334002b8cc5c434
JinoSejera <jin.sejera22@gmail.com>
2025-03-24 15:29:29 +0800

    re config logging

--- app/main.py
+++ app/main.py
@@ -12,7 +12,12 @@ import time
 from dotenv import load_dotenv
 
 load_dotenv()
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    force=True
+    )
 logger = logging.getLogger(__name__)
 
 app = FastAPI()
