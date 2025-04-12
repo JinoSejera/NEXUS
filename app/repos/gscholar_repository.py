3edb96b28c8d2433d31a7983409548a4df26b101
JinoSejera <jin.sejera22@gmail.com>
2025-04-12 23:10:02 +0800

    code refinement

--- app/repos/gscholar_repository.py
+++ app/repos/gscholar_repository.py
@@ -1,11 +1,11 @@
 import aiohttp
 from bs4 import BeautifulSoup
 from fastapi import HTTPException
-from typing import List,Dict
+from typing import List, Dict
 
 class GoogleScholarRepository:
     @staticmethod
-    async def search_rrl(query:str, rrl_number:int):
+    async def search_rrl(query: str, rrl_number: int) -> List[Dict[str, str]]:
         uri = f"https://scholar.google.com/scholar?q={query}"
         
         headers = {
@@ -18,19 +18,26 @@ class GoogleScholarRepository:
                     
                     text = await response.text()
                     
-                    soup = BeautifulSoup(text,'html.parser')
+                    soup = BeautifulSoup(text, 'html.parser')
                     result = []
                     for item in soup.find_all('div', class_='gs_ri'):
-                        title = item.find('h3', class_='gs_rt').text
-                        link = item.find('a')['href']
-                        result.append({
-                            "title": title,
-                            "link": link
-                        })
+                        title_tag = item.find('h3', class_='gs_rt')
+                        link_tag = item.find('a')
+                        
+                        if title_tag and link_tag:
+                            title = title_tag.text
+                            link = link_tag['href']
+                            result.append({
+                                "title": title,
+                                "link": link
+                            })
                     
                     return result[:rrl_number]
+            except aiohttp.ClientError as e:
+                raise HTTPException(status_code=500,
+                                    detail=f"HTTP error while fetching data from Google Scholar: {str(e)}")
             except Exception as e:
                 raise HTTPException(status_code=500,
-                                    detail=f"Error fetching data from Google Scholar: {str(e)}")
+                                    detail=f"Unexpected error: {str(e)}")
             finally:
                 await session.close()
